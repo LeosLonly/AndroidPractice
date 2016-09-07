ffc15785b07f523c077dbc303e5cfe5672108ac8
LeosLonely <18813080818@163.com>
2016-09-08 07:57:41 +0800

    Add bindservicesapp Modle

--- bindservicesapp/src/main/java/edu/bistu/computer/bindservicesapp/MainActivity.java
+++ bindservicesapp/src/main/java/edu/bistu/computer/bindservicesapp/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivityTag";
+    private static final String TAG = "MyServicesTag";
     private MyService service;
 
     @Override
